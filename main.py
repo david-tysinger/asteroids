7b431dc459e6e8d5d297ee30a302a8ac1acafae7
David Tysinger <davidtysinger@gmail.com>
2025-10-15 01:47:48 +0000

    added framerate calculation

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -21,6 +24,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
