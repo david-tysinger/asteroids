b267a3661e8e77744913e98597157c7a6a23f7cc
David Tysinger <davidtysinger@gmail.com>
2025-12-24 23:42:57 +0000

    corrected logging

--- main.py
+++ main.py
@@ -27,18 +27,19 @@ def main():
     #initialize delta time variable
     dt = 0
 
-    #call logger
-    log_state()
+
 
     #set loop to draw screen
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        log_state()
         screen.fill((0, 0, 0))
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
